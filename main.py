018b193ec6c2ffe33944787c4d7d9555e4a859a0
jadenwilson-ux <jaden_wilson@paraparaumucollege.school.nz>
2026-05-04 13:01:00 +1200

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import supporting_module
 # Introduction
 name = input("Hello and welcome to the interactive LED tool, please enter your name: ")
 
+# Opening brief.txt and parsing its paragraphs into a list
 with open("brief.txt", "r") as f:
     content = f.read()
 paragraphs = content.split("\n\n")
 
+print("To exit the menu press 0")
+print("To learn about LEDs press 1")
+print("To learn about Ohm's Law press 2")
+print("To calculate the appropriate resistor press 3")
+# Looping the menu until users are satisfied and press 0 to exit the menu
 while True:
     try:
-        print("To exit the menu press 0")
-        print("To learn about LEDs press 1")
-        print("To learn about Ohm's Law press 2")
-        selection = int(input("To calculate the appropriate resistor press 3\n> "))
+        selection = int(input("> "))
         if selection == 0:
             break
         else:
@@ -26,6 +29,3 @@ circuits = supporting_module.load_circuits("datavalues.json")
 score, hints_used = supporting_module.run_quiz(circuits)
 print(score)
 print(hints_used)
-
-
-
